91e8fafd03bdb88be290c2ca2218534621659add
Oscar Cisneros <cisneros_olcj@outlook.com>
2014-07-23 19:48:59 -0500

    implimeented update and destroy actions

--- app/controllers/prospects_controller.rb
+++ app/controllers/prospects_controller.rb
@@ -14,12 +14,29 @@ class ProspectsController < ApplicationController
             :first_name => params[:first_name],:last_name=>params[:last_name],:team=>params[:team],:position=>params[:position],:rank=>params[:rank]
             }
             }).body
- 
-    redirect_to prospects_path(@prospect["id"])                        
-  end
-  def update
-  
+    puts @prospect
+    redirect_to prospect_path(@prospect["id"]) 
 
   end
+  def edit  
+     @prospect = Unirest.get("http://localhost:3001/prospects/#{params[:id]}.json", :headers => {"Accept"=>"application/json"}).body
+  end
+
+  def update 
+    @prospect = Unirest.patch("http://localhost:3001/prospects/#{params[:id]}.json", :headers => {"Accept"=>"application/json"}, headers:{"Accepts"=>"application/json"},
+      parameters:{ :prospect =>
+        {
+          :first_name => params[:first_name],:last_name=>params[:last_name],:team=>params[:team],:position=>params[:position],:rank=>params[:rank]
+        }
+      }
+      ).body
+    redirect_to prospect_path
+  end
+  def destroy
+    Unirest.delete("http://localhost:3001/prospects/#{params[:id]}.json", :headers => {"Accept"=>"application/json"},)
+    redirect_to prospects_path
+  
+  end
+
 
 end
